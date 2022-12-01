ebd910c6ef9bf2c84df163f0eba9a9a7b6f89183
trsalondra <trsalondra@gmail.com>
2022-12-01 14:39:23 -0600

    index.jsx view rendered at pokemon route again

--- server.js
+++ server.js
@@ -5,34 +5,13 @@ const port = 3000
 const fs = require('fs')
 const pokemon = require('./models/pokemon.js')
 
-app.set('views', './views')
 app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
-app.engine('jsx', (filePath, options, callback) => { // 
-    fs.readFile(filePath, (err, content) => {
-      if (err) return callback(err)
-      const rendered = content.toString()
-
-    //     .replace('#title#', '<title>' + options.title + '</title>')
-    //     .replace('#title#', '<h1>' + options.title + '</h1>')
-    //     .replace('#subtitle#', '<h2>' + options.subtitle + '</h2>')
-    //     .replace('#image#', '<img src="' + options.imageLink + '">')
-    //     .replace('#description#', '<h3>' + options.description + '</h3>')
-    //     .replace('#servingTime#', '<h3>Serving Time: ' + options.servingTime + '</h3>')
-    //     .replace('#level#', '<h3>Level: ' + options.level + '</h3>')
-    //     .replace('#ingredients#','<h3>Ingredients</h3><ul><li>'+ options.ingredients.join('</li><br><li>') + '</li><br></li></ul>')
-    //     .replace('#supplies#','<h3>What youll need</h3><ul><li>'+ options.supplies.join('</li><br><li>') + '</li><br></li></ul>')
-    //     .replace('#steps#','<h3>Directions</h3><ol><li>'+ options.steps.join('</li><br><li>') + '</li><br></li></ol>')
-
-      return callback(null, rendered)
-    })
-  })
-
-app.get('/', (req, res) => {
-    
-})
+app.get('/', (req, res) => { })
 
 app.get('/pokemon/', (req, res) => {
+    // res.send(pokemon)
     res.render('Index')
 })
 app.listen(port, () => {
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=<h>, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    <h1 style="color:blue" >See All The Pokemon!</h1>
-</body>
-</html>
+
+const React = require('react')
+
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+}
+
+class MyFirstComponent extends React.Component {
+    render() {
+        return (<div style={myStyle}>My First React Component!</div>)
+    }
+}
+
+module.exports = MyFirstComponent
